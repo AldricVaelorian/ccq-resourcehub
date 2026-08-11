2cf7972331d7cf6093d3d02911332ddc2650b1a4
Aldric Vaelorian <30434580+AldricVaelorian@users.noreply.github.com>
2026-08-11 09:21:20 +0000

    test: enable migrations for concurrency test

--- backend/src/test/java/de/ccq/resourcehub/service/BookingApprovalConcurrencyIntegrationTest.java
+++ backend/src/test/java/de/ccq/resourcehub/service/BookingApprovalConcurrencyIntegrationTest.java
@@ -30,6 +30,7 @@ class BookingApprovalConcurrencyIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.flyway.enabled", () -> "true");
     }
 
     @Autowired
